c5e423435b9a63131659de7861f1381289dca8ae
Vaibhav1015 <vaibhavandhale3510@gmail.com>
2023-08-02 16:50:36 +0530

    added numberTowords function and changes post api

--- index.js
+++ index.js
@@ -1,90 +1,128 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const puppeteer = require('puppeteer');
-const ejs = require('ejs');
-const fs = require('fs');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const puppeteer = require("puppeteer");
+const ejs = require("ejs");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 
 // Connect to MongoDB (Replace 'mongodb://localhost/bill-receipt' with your MongoDB connection string)
-mongoose.connect('mongodb+srv://vaibhav10:Vaibhav1015@cluster0.on5gazj.mongodb.net/bill-receipt', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-const Bill = require('./models/bill'); // Assuming you have a Bill model for MongoDB
+const Bill = require("./models/bill"); // Assuming you have a Bill model for MongoDB
+const { price_in_words } = require("./numberToWords");
 
 // Middleware to parse JSON in request body
 app.use(express.json());
 
 // post api new invoice
-app.post('/add_new_invoice',async (req,res) => {
-    try {
-        // const newInvoiceData = req.body;
-       const dataInvoice = new Bill(req.body)
-       if(dataInvoice){
-        const saveData = dataInvoice.save()
-        res.status(200).send({msg:'success',invoice:dataInvoice})
-       } else{
-        res.status(400).send({msg:'Something went wrong!!'})
-       }
-    } catch (error) {
-        return res.status(500).json({ error: 'Something went wrong' });
-    }
-})
+app.post("/add_new_invoice", async (req, res) => {
+  try {
+    const billingAddress = req.body.billingAddress;
+    const deliveryAddress = req.body.deliveryAddress;
+    const items = req.body.items;
+    items.forEach((item) => {
+      item.amount = item.rate * item.quantity;
+    });
 
+    const netTotalAmount = items.reduce(
+      (total, item) => total + item.amount,
+      0
+    );
+    const netTotal = netTotalAmount;
+    const percentCGst = req.query.cGst;
+    const percentSGst = req.query.sGst;
+    const percentIGst = req.query.iGst;
+    const totalCGst = Math.ceil(
+      (netTotal / 100) * (percentCGst ? percentCGst : 0)
+    );
+    const totalSGst = Math.ceil(
+      (netTotal / 100) * (percentSGst ? percentSGst : 0)
+    );
+    const totalIGst = Math.ceil(
+      (netTotal / 100) * (percentIGst ? percentIGst : 0)
+    );
+    const grandTotal = netTotal + totalCGst + totalIGst + totalSGst;
+    const totalAmountInWords = price_in_words(grandTotal);
+    const dataInvoice = new Bill({
+      billingAddress: billingAddress,
+      deliveryAddress: deliveryAddress,
+      items: items,
+      netTotal: netTotal,
+      cGst: totalCGst,
+      sGst: totalSGst,
+      iGst: totalIGst,
+      grandTotal: grandTotal,
+      totalAmountInWords: totalAmountInWords,
+    });
+    if (dataInvoice) {
+      const saveData = dataInvoice.save();
+      res.status(200).send({ msg: "success", invoice: dataInvoice });
+    } else {
+      res.status(400).send({ msg: "Something went wrong!!" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+});
 
 // API endpoint to generate and download the bill receipt PDF
-app.get('/generateBillPDF/:billId', async (req, res) => {
+app.get("/generateBillPDF/:billId", async (req, res) => {
   try {
     const billId = req.params.billId;
-    const bill = await Bill.findOne({_id:billId});
+    const bill = await Bill.findOne({ _id: billId });
 
     if (!bill) {
-      return res.status(404).json({ error: 'Bill not found' });
+      return res.status(404).json({ error: "Bill not found" });
     }
 
-    const html = await ejs.renderFile('templates/index.ejs', {
-        deliveryAddress:bill.deliveryAddress.address,
-        deliveryTel:bill.deliveryAddress.tel,
-        deliveryGst:bill.deliveryAddress.gstNo,
-        billingAddress:bill.billingAddress.address,
-        billingTel:bill.billingAddress.tel,
-        billingGst:bill.billingAddress.gstNo,
-        items: bill.items,
-        netTotal:bill.netTotal,
-        grandTotal:bill.grandTotal,
-        cGst:bill.cGst,
-        iGst:bill.iGst,
-        sGst:bill.sGst,
-        totalAmountInWords:bill.totalAmountInWords,
-
+    const html = await ejs.renderFile("templates/index.ejs", {
+      deliveryAddress: bill.deliveryAddress.address,
+      deliveryTel: bill.deliveryAddress.tel,
+      deliveryGst: bill.deliveryAddress.gstNo,
+      billingAddress: bill.billingAddress.address,
+      billingTel: bill.billingAddress.tel,
+      billingGst: bill.billingAddress.gstNo,
+      items: bill.items,
+      netTotal: bill.netTotal,
+      grandTotal: bill.grandTotal,
+      cGst: bill.cGst,
+      iGst: bill.iGst,
+      sGst: bill.sGst,
+      totalAmountInWords: bill.totalAmountInWords,
     });
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.setContent(html, {
-      waitUntil: 'domcontentloaded',
+      waitUntil: "domcontentloaded",
     });
 
-    const pdfBuffer = await page.pdf({format:'A4'});
+    const pdfBuffer = await page.pdf({ format: "A4" });
     await browser.close();
 
     const pdfFileName = `bill_receipt_${billId}.pdf`;
     fs.writeFileSync(`public/${pdfFileName}`, pdfBuffer);
 
-    return res.json({ message: 'PDF generated successfully', pdfUrl: `/download/${pdfFileName}` });
+    return res.json({
+      message: "PDF generated successfully",
+      pdfUrl: `/download/${pdfFileName}`,
+    });
   } catch (err) {
-    console.error('Error generating PDF:', err);
-    return res.status(500).json({ error: 'Something went wrong' });
+    console.error("Error generating PDF:", err);
+    return res.status(500).json({ error: "Something went wrong" });
   }
 });
 
 // Serve the PDF files
-app.use('/download', express.static('public'));
+app.use("/download", express.static("public"));
 
 // Start the server
 app.listen(port, () => {
--- models/bill.js
+++ models/bill.js
@@ -1,70 +1,77 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const billSchema = new mongoose.Schema({
-    billingAddress: {
-        address: {
-          type: String,
+  billingAddress: {
+    address: {
+      type: String,
+      //   required: true,
+    },
+    tel: {
+      type: String,
+      //   required: true,
+    },
+    gstNo: {
+      type: String,
+      //   required: true,
+    },
+  },
+  deliveryAddress: {
+    address: {
+      type: String,
+      //   required: true,
+    },
+    tel: {
+      type: String,
+      //   required: true,
+    },
+    gstNo: {
+      type: String,
+      //   required: true,
+    },
+  },
+  items: [
+    {
+      description: {
+        type: String,
         //   required: true,
-        },
-        tel: {
-          type: String,
-        //   required: true,
-        },
-        gstNo: {
-          type: String,
-        //   required: true,
-        },
       },
-      deliveryAddress: {
-        address: {
-          type: String,
-        //   required: true,
-        },
-        tel: {
-          type: String,
-        //   required: true,
-        },
-        gstNo: {
-          type: String,
-        //   required: true,
-        },
+      hsnCode: {
+        type: String,
       },
-  items: [{
-    description: {
-      type: String,
-    //   required: true,
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      rate: {
+        type: Number,
+        required: true,
+      },
+      amount: {
+        type: Number,
+        //   required: true,
+        default: 0,
+      },
     },
-    hsnCode: {
-      type: String,
-    },
-    quantity: {
-      type: String,
-    //   required: true,
-    },
-    rate: {
-      type: Number,
-    //   required: true,
-    },
-    amount: {
-      type: Number,
-    //   required: true,
-    },
-  }],
+  ],
   netTotal: {
     type: Number,
     // required: true,
+    default: 0,
   },
   cGst: {
     type: Number,
     // required: true,
+    default: 0,
   },
   sGst: {
     type: Number,
     // required: true,
+    default: 0,
   },
   iGst: {
     type: Number,
     // required: true,
+    default: 0,
   },
   grandTotal: {
     type: Number,
@@ -73,9 +80,9 @@ const billSchema = new mongoose.Schema({
   totalAmountInWords: {
     type: String,
     // required: true,
-  }
+  },
 });
 
-const Bill = mongoose.model('Bill', billSchema);
+const Bill = mongoose.model("Bill", billSchema);
 
 module.exports = Bill;
